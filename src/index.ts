b9ad423a03b3043989a297cdb779570d66ce2e60
Harsh Banka <harshbanka321@gmail.com>
2023-09-10 15:41:23 +0530

    feat: add mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import Loaders from './loaders';
+import config from './config';
 
 async function startServer() {
   const app = express();
   await Loaders({ expressApp: app });
   app
-    .listen(3000, () => {
+    .listen(config.port, () => {
       console.log(`
-      ################################################
-      Server listening on port: ${3000} 
-      ################################################
+      <===== Server listening on port: ${config.port} =====>
     `);
     })
     .on('error', (err: Error) => {
--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,7 +1,10 @@
 import express from "./express";
+import mongo from "./mongo";
 import Express from "express";
 
 export default async ({ expressApp}: { expressApp: Express.Application }): Promise<void> => {
+    await mongo();
+    console.log("MongoDB Intialized");
     await express({ app: expressApp });
     console.log("Express App Intialized");
 
