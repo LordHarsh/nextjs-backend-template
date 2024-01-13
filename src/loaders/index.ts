d10a5a5af0a438a79b806b29723edfdd8bb6a531
LordHarsh <harshhbanka321@gmail.com>
2024-01-13 17:57:45 +0530

    feat: add shared middlewares

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,12 +1,12 @@
 import express from "./express";
 import mongo from "./mongo";
 import Express from "express";
+import LoggerInstance from "./logger";
 
 export default async ({ expressApp}: { expressApp: Express.Application }): Promise<void> => {
     await mongo();
-    console.log("MongoDB Intialized");
+    LoggerInstance.info("MongoDB Intialized");
     await express({ app: expressApp });
-    console.log("Express App Intialized");
-
-    console.log("All modules loaded!");
-}
+    LoggerInstance.info("Express App Intialized");
+    LoggerInstance.info("All modules loaded!");
+};
